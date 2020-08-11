fed6be4bdd407e4941624c703b42b989576d2a5c
AndreiAnton <38466763+AndreiAnton@users.noreply.github.com>
2020-08-12 00:04:35 +0300

    Revert "Transaction list"

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import React from 'react';
-import { Header } from './components/Header';
-import { Balance } from './components/Balance';
-import { IncomeExpenses } from './components/IncomeExpenses';
-import { TransactionList } from './components/TransactionList';
-
 import './App.css';
 
 function App() {
   return (
     <div>
-      <Header />
-      <div className="container">
-        <Balance />
-        <IncomeExpenses />
-        <TransactionList />
-      </div>
+      Hello World
     </div>
   );
 }
