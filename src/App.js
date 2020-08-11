22ae8cfdbdc6cc0906ff54f8014c48046058e980
AndreiAnton <38466763+AndreiAnton@users.noreply.github.com>
2020-08-11 23:37:49 +0300

    Revert "Header component"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import Header from './components/Header';
-
 import './App.css';
 
 function App() {
   return (
     <div>
-      <Header />
+      Hello World
     </div>
   );
 }
