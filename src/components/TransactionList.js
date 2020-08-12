3a568dafc98f71a548b28d1c5a9695c452e13273
Andrei <anton.andreii@example.com>
2020-08-12 23:11:40 +0300

    using global context in transaction list

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 export const TransactionList = () => {
+    const { transactions } = useContext(GlobalContext);
+
+    console.log(transactions);
+
     return (
         <>
             <h3>History</h3>
             <ul className="list">
-                <li className="minus">
-                    Cash <span>-$400</span><button className="delete-btn">x</button>
-                </li>
+                {transactions.map(transaction => (<li className="minus">
+                    {transaction.text} <span>-$400</span><button className="delete-btn">x</button>
+                </li> ))}
             </ul>
         </>
     )
